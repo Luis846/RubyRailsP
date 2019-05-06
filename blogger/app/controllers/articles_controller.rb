a567f57a195c576d8d0b407d885980c83e1342c8
Luis <luis.rivera846@gmail.com>
2019-05-06 18:41:19 -0400

    added comment section

--- blogger/app/controllers/articles_controller.rb
+++ blogger/app/controllers/articles_controller.rb
@@ -8,6 +8,8 @@ class ArticlesController < ApplicationController
     end
     def show
         @article = Article.find(params[:id])
+        @comment = Comment.new
+        @comment.article_id = @article.id
     end
 
     def edit
